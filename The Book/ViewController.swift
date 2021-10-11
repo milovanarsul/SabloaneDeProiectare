592e901f1d8136c6c6c77d05888899f554d3da36
Marian Arsul <marian.arsul00@e-uvt.ro>
2021-10-11 12:12:10 +0300

    Initial commit

---
+++ The Book/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Book
+//
+//  Created by Milovan Arsul on 11.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
