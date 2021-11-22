ede0063e7ff02ebb1be9266e399e3ecf6ed84abd
Marian Arsul <marian.arsul00@e-uvt.ro>
2021-11-22 13:27:02 +0200

    Lab5: Singleton Design Pattern

--- The Book/ViewController.swift
+++ The Book/ViewController.swift
@@ -9,8 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    public func Printing(){
+        DocumentManager.getInstance().getBook().print()
+    }
+    
     private func main(){
+        let myBook = Book(title: "My Book")
+        DocumentManager.getInstance().setBook(book: myBook)
+        
+        let me = Author(name: "Myself", surname: "")
+        myBook.addAuthor(author: me)
+        
         let cap1 = Section(sectionTitle: "Capitolul 1")
+        myBook.addContent(content: cap1)
         
         let p1 = Paragraph(paragraphText: "Paragraph 1")
         cap1.add(element: p1)
@@ -18,7 +29,7 @@ class ViewController: UIViewController {
         let p2 = Paragraph(paragraphText: "Paragraph 2")
         cap1.add(element: p2)
         
-        let p3 = Paragraph(paragraphText: "Paragraph 3")
+        let p3 = Paragraph(paragraphText: "Paragraph 1")
         cap1.add(element: p3)
         
         let p4 = Paragraph(paragraphText: "Paragraph 4")
@@ -31,8 +42,10 @@ class ViewController: UIViewController {
         p2.setAlignStrategy(strategy: AlignRight()!)
         p3.setAlignStrategy(strategy: AlignLeft()!)
         
-        Swift.print("\n Printing with Alignment \n")
+        Swift.print("\nPrinting with Alignment \n")
         cap1.print()
+        
+        Printing()
     }
 
     override func viewDidLoad(){
